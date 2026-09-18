c5747c3a65ed49f0c196c86f2e2223832a2b2289
Net09 <Net09@511-12>
2026-09-18 17:57:35 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿//double x = float.Parse(Console.ReadLine());
+//float y = (float)(2 * x);
+//    int a=567;
+//byte b = (byte)a;
+//Console.WriteLine(b);
+ 
---
+++ ConsoleApp1/lab1_2.cs
@@ -0,0 +1,33 @@
+﻿//try
+//{
+//    Console.Write("Введите первую переменную");
+//    double x = double.Parse(Console.ReadLine());
+//    Console.Write("Введите вторую переменную");
+//    double y = double.Parse(Console.ReadLine());
+//    double Avg = (Math.Pow(x, 3) + Math.Pow(y, 3)) / 2;
+//    Console.WriteLine($"Среднее арифметическое:{Avg:F2}");
+
+//        }
+//catch(Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+
+
+using System.Runtime.Intrinsics.X86;
+
+try
+{
+    Console.Write("Введите первую переменную");
+    double L = double.Parse(Console.ReadLine());
+    Console.Write("Введитt вторую переменную");
+    double I = double.Parse(Console.ReadLine());
+   double W = (0.5*L)*Math.Pow(I,2);
+    Console.WriteLine($"Среднее арифметическое:{W:F2}");
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
